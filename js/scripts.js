2a9af7be20c12c973099e23670a7ba77422a4fd2
Darrion Gering and Mathew Akre <darrion@>
2019-04-02 14:33:40 -0700

    add 3 form group questions about stress, js jquery forEach function, if statments in each function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,53 @@
 $(document).ready(function() {
-  $("#transportation-survey").submit(function(event) {
-    var userResponses = [];
-    $("input:checkbox[name=work-transportation]:checked").each(function() {
-      var workTransportationMode = $(this).val();
-       userResponses.push(workTransportationMode);
-       console.log("test1");
+  $("#stress-test").submit(function(event) {
+      event.preventDefault();
+      var negativeStressCounter = 0;
+      var helpingMethodsCounter = 0;
+      var copingMethodsCounter = 0;
+    $("input:checkbox[name=negativeStress]:checked").each(function() {
+      var helpNegativeStress = $(this).val();
+      if(helpNegativeStress === "sleep") {
+        negativeStressCounter +=1;
+      } else if(helpNegativeStress === "overwhelmed") {
+        negativeStressCounter +=1;
+      } else if(helpNegativeStress === "baby") {
+        negativeStressCounter +=1;
+      }
     });
-    userResponses.forEach(function(response) {
-    $('#work-responses').append(response + "<br>");
+    $("input:checkbox[name=healthSymptoms]:checked").each(function() {
+      console.log("test")
+      var helpHealthSymptoms = $(this).val();
+      console.log("test1")
+      if(helpHealthSymptoms === "highBloodPressure") {
+        helpingMethodsCounter +=1;
+      } else if(helpHealthSymptoms === "paranoia") {
+        helpingMethodsCounter +=1;
+      } else if(helpHealthSymptoms === "anxiety") {
+        helpingMethodsCounter +=1;
+      } else if(helpHealthSymptoms === "sleepApnea") {
+        helpingMethodsCounter +=1;
+      }
+      console.log("test2")
     });
-    console.log("test2");
-    event.preventDefault();
+    $("input:checkbox[name=copingMethods]:checked").each(function() {
+      var helpCopingMethods = $(this).val();
+      if(helpCopingMethods === "meditate") {
+        copingMethodsCounter +=1;
+      } else if(helpCopingMethods === "timeManagement") {
+        copingMethodsCounter +=1;
+      } else if(helpCopingMethods === "petDog") {
+        copingMethodsCounter +=1;
+      } else if(helpCopingMethods === "stressBall") {
+        copingMethodsCounter +=1;
+      }
+    });
+
+    if(copingMethodsCounter > (negativeStressCounter + helpingMethodsCounter)) {
+      $("#help-response").append("You're doing a great job managing your stress!");
+    } else if (copingMethodsCounter === (negativeStressCounter + helpingMethodsCounter)) {
+      $("#help-response").append("Find a dog and start petting!");
+    } else {
+      $("#help-response").append("You suck at managing your stress!");
+    }
   });
 });
